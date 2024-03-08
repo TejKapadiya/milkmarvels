a0f40ad5c8ab0ee74488d585056d6fec67e5c6a7
Heet Jambudiya <heet0504@gmail.com>
2024-03-08 23:04:17 +0530

    js done
    30.27

--- APP/views.py
+++ APP/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+import os
 # Create your views here.
+# def login(request):
+#     return render(request,"home.html")
+
+def home(request):
+    return render(request,"home.html")
+
+
+def cart(request):
+    return render(request,"cart.html")
