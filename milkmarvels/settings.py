9b8c9d0e52c67844969a0d4cb8aa4f8c5f5d10bb
Tej_Kapadiya <tejkapadiya10@gmail.com>
2024-03-08 17:38:55 +0530

    database

--- milkmarvels/settings.py
+++ milkmarvels/settings.py
@@ -72,15 +72,28 @@ TEMPLATES = [
 WSGI_APPLICATION = 'milkmarvels.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# # Database
+# # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sp_mm',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
+
+
 
 
 # Password validation
