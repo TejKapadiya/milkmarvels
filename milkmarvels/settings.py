6cbf164db1dcfd74408477feadcffc3d0b5a1d91
Heet Jambudiya <heet0504@gmail.com>
2024-03-08 18:20:22 +0530

    Update settings.py
    Updated database

--- milkmarvels/settings.py
+++ milkmarvels/settings.py
@@ -85,10 +85,10 @@ WSGI_APPLICATION = 'milkmarvels.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sp_mm',
+        'NAME': 'mm_py',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '3306',
     }
 }
