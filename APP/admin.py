959f74e3a2f7d258a151e7e6811f6c49252b8686
Tej_Kapadiya <tejkapadiya10@gmail.com>
2024-03-31 09:21:01 +0530

    1:01

--- APP/admin.py
+++ APP/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from .models import Product, User, Cart, Orders, Coupon
+from .models import Product
+# , User, Cart, Orders, Coupon
 
 # Register your models here.
 
 admin.site.register(Product)
-admin.site.register(User)
-admin.site.register(Cart)
-admin.site.register(Orders)
-admin.site.register(Coupon)
+# admin.site.register(User)
+# admin.site.register(Cart)
+# admin.site.register(Orders)
+# admin.site.register(Coupon)
 
 # Register your models here.
--- APP/models.py
+++ APP/models.py
@@ -1,41 +1,65 @@
+# from django.db import models
+
+# # Create your models here.
+# from django.contrib.auth.models import *
+
+# from django.db import models
+
+# class Product(models.Model):
+#     product_id = models.AutoField(primary_key=True)
+#     name = models.CharField(max_length=255)
+#     description = models.TextField(blank=True, null=True)
+#     price = models.FloatField()
+#     expiry_date = models.DateField()
+#     quantity = models.IntegerField()
+
+# class User(models.Model):
+#     user_id = models.AutoField(primary_key=True)
+#     name = models.CharField(max_length=255)
+#     address = models.TextField()
+#     contact = models.CharField(max_length=20)
+#     password = models.CharField(max_length=255)
+#     email = models.EmailField()
+
+# class Cart(models.Model):
+#     cart_cookie_id = models.AutoField(primary_key=True)
+#     total_amount = models.FloatField()
+
+# class Orders(models.Model):
+#     order_id = models.AutoField(primary_key=True)
+#     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+#     total_amount = models.FloatField()
+#     delivery_by_date = models.DateField()
+#     status = models.CharField(max_length=255)
+#     payment_details = models.TextField()
+#     transaction_id = models.CharField(max_length=255)
+#     coupon_id = models.ForeignKey('Coupon', on_delete=models.SET_NULL, null=True)
+
+# class Coupon(models.Model):
+#     coupon_id = models.AutoField(primary_key=True)
+#     amount = models.FloatField()
+#     coupon_name = models.CharField(default="abc", max_length=25)
+
 from django.db import models
 
-# Create your models here.
-from django.contrib.auth.models import *
-
-from django.db import models
-
+CATEGORY_CHOICES=[
+    ('CR', 'Curd'),
+    ('ML', 'Milk'),
+    ('LS', 'Lassi'),
+    ('MS', 'Milkshake'),
+    ('PN', 'Paneer'),
+    ('GH', 'Ghee'),
+    ('CZ', 'Cheese'),
+    ('IC', 'Ice-Creams'),
+]
 class Product(models.Model):
-    product_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-    price = models.FloatField()
-    expiry_date = models.DateField()
-    quantity = models.IntegerField()
-
-class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    address = models.TextField()
-    contact = models.CharField(max_length=20)
-    password = models.CharField(max_length=255)
-    email = models.EmailField()
-
-class Cart(models.Model):
-    cart_cookie_id = models.AutoField(primary_key=True)
-    total_amount = models.FloatField()
-
-class Orders(models.Model):
-    order_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    total_amount = models.FloatField()
-    delivery_by_date = models.DateField()
-    status = models.CharField(max_length=255)
-    payment_details = models.TextField()
-    transaction_id = models.CharField(max_length=255)
-    coupon_id = models.ForeignKey('Coupon', on_delete=models.SET_NULL, null=True)
-
-class Coupon(models.Model):
-    coupon_id = models.AutoField(primary_key=True)
-    amount = models.FloatField()
-    coupon_name = models.CharField(default="abc", max_length=25)
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    composition = models.TextField(default='')
+    prodapp = models.TextField(default='')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=20)
+    product_image = models.ImageField(upload_to='product')
+    def __str__(self):
+        return self.title
